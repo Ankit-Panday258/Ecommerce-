dc74dddf475a8872b274a8a4d81a7d539876994b
ankit258 <123321233+Ankit-Panday258@users.noreply.github.com>
2023-12-04 17:23:59 +0530

    Complete Description

---
+++ src/comput/DescriptionBox/DescriptionBox.js
@@ -0,0 +1,17 @@
+import './DescriptionBox.css'
+const  DescriptionBox = () =>{
+    return(
+        <div className="DescriptionBox">
+            <div className='DescriptionBox-navigator'>
+                <div className='DescriptionBox-nav-Box'>Description</div>
+                <div className='DescriptionBox-nav-Box fade'>Reviews (122)</div>
+            </div>
+            <div className='DescriptionBox-Description'>
+                <p>E-commerce has a global reach, allowing businesses to expand their customer base beyond geographical boundaries. It has also led to the emergence of various business models, such as dropshipping, subscription services, and online marketplaces. E-commerce continues to evolve with advancements in technology, including the integration of artificial intelligence, augmented reality, and virtual reality to enhance the online shopping experience.</p>
+                <p>With the widespread use of smartphones, many e-commerce transactions take place through mobile devices, requiring platforms to optimize for mobile user experiences.</p>
+            </div>
+        </div>
+    )
+}
+export default DescriptionBox;
+
